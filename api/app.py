f683b308035056090b5b0306e03cf38690aa1190
Timothy Karani <kartimothy@gmail.com>
2021-05-27 21:48:16 +0300

    Added multipart

--- api/app.py
+++ api/app.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
+
+ouath2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/")
+async def read_items(token: str = Depends(ouath2_scheme)):
+    return {"token": token}
