5fc020c6cd41a30b53c3b093a85cc3bb3d85fdaf
Timothy Karani <kartimothy@gmail.com>
2021-05-27 22:28:26 +0300

    added user model

--- api/app.py
+++ api/app.py
@@ -1,9 +1,19 @@
+from typing import Optional
+
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 app = FastAPI()
 
 ouath2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
 
 
 @app.get("/")
