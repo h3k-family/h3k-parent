5511748422a403fcc832d60f59268d84cb4a7c7c
Timothy Karani <kartimothy@gmail.com>
2021-05-28 22:28:55 +0300

    Removed unneeded code

--- api/app.py
+++ api/app.py
@@ -2,21 +2,6 @@ import os
 from fastapi import FastAPI
 from auth import auth
 
-# openssl rand -hex 32
-SECRET_KEY = "9a4dbb8b7e5eff75422194bf5820b010607e29819520106db6617445c537d800"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
 app = FastAPI(
     title="h3k-child API",
     description="Haba na haba, hujaza kibaba. Part of the h3k project.",
