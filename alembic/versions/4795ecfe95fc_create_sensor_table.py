ed4eb2eea7bc4d10851621a8652c16bc9ae8650d
Timothy Karani <kartimothy@gmail.com>
2021-05-29 00:29:40 +0300

    Added sensor table

--- alembic/versions/4795ecfe95fc_create_sensor_table.py
+++ alembic/versions/4795ecfe95fc_create_sensor_table.py
@@ -8,6 +8,8 @@ Create Date: 2021-05-29 00:16:28.400846
 from alembic import op
 import sqlalchemy as sa
 
+from sqlalchemy.sql import expression
+from sqlalchemy.ext.compiler import compiles
 
 # revision identifiers, used by Alembic.
 revision = '4795ecfe95fc'
@@ -15,10 +17,29 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+# https://stackoverflow.com/a/33532154
+# https://docs.sqlalchemy.org/en/14/core/compiler.html#further-examples
+
+
+class utcnow(expression.FunctionElement):
+    type = sa.DateTime()
+
+
+@compiles(utcnow, "postgresql")
+def pg_utcnow(element, compiler, **kw):
+    return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
+
 
 def upgrade():
-    pass
+    op.create_table(
+        "sensor_details",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("sensor_name", sa.String(100)),
+        sa.Column("units_short", sa.String(20)),
+        sa.Column("units_long", sa.String(50)),
+        sa.Column("updated_at", sa.DateTime, server_default=utcnow()),
+    )
 
 
 def downgrade():
-    pass
+    op.drop_table("sensor_details")
