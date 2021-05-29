e84105fa6acab9c418951f4491e0131e40b6d21d
Timothy Karani <kartimothy@gmail.com>
2021-05-29 03:42:40 +0300

    Moved models to own file

--- auth/auth.py
+++ auth/auth.py
@@ -8,43 +8,15 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
+import auth.models as mo
+
+
 router = APIRouter()
 
 # openssl rand -hex 32
 SECRET_KEY = str(uuid.uuid4())
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-
-class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -71,7 +43,7 @@ def authenticate_user(fake_db, username: str, password: str):
 def get_user(database, username: str):
     if username in database:
         user_dict = database[username]
-        return UserInDB(**user_dict)
+        return mo.UserInDB(**user_dict)
 
     return False
 
@@ -102,13 +74,13 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except JWTError as jwt_exc:
         raise credentials_exception from jwt_exc
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(mo.fake_users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
 
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
+async def get_current_active_user(current_user: mo.User = Depends(get_current_user)):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
@@ -117,7 +89,7 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
 @router.post("/token", tags=["auth"])
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(
-        fake_users_db, form_data.username, form_data.password)
+        mo.fake_users_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -133,10 +105,10 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
 
 
 @router.get("/users/me/", tags=["auth"])
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
+async def read_users_me(current_user: mo.User = Depends(get_current_active_user)):
     return current_user
 
 
 @router.get("/users/me/items/", tags=["auth"])
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
+async def read_own_items(current_user: mo.User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
---
+++ auth/models.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from pydantic import BaseModel
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
